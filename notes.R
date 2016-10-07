fb18faf2b1543911ff9093901e90b919ca8c9d72
smdanna <sofia.m.danna@gmail.com>
2016-10-07 14:53:54 -0400

    Update notes.R

--- notes.R
+++ notes.R
@@ -1,10 +1,10 @@
-# count of non-NAs in dataframe “cog”
+# count of non-NAs in dataframe "cog"
 colSums(!is.na(cog))
 
 # write first 50 rows of dataset to csv file 
 # w2ktn <- w2[c(1:50),]
 View(w2ktn)
-write.csv(w2ktn, “w2ktn.csv”, row.names=TRUE)
+write.csv(w2ktn, "w2ktn.csv", row.names=TRUE)
 
 # get column index from dataframe
 which(colnames(w2ktn)=="TDIABET")
